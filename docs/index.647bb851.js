b019b46ce6b73e4069aec135888514737dc0ead5
Oliver Ponder <oliver.ponder@gmail.com>
2023-09-17 00:08:36 +0200

    Output to docs folder

---
+++ docs/index.647bb851.js
@@ -0,0 +1,6 @@
+scrollArea=document.getElementById("scrollArea"),// Call the animate function to start the animation loop
+// Set up an animation loop
+function e(){var l;console.log(l=new Date().getTime()),scrollArea.style.right=l/50%3600+"px",// Call the animate function again next time the browser is ready to draw
+requestAnimationFrame(e)}();//# sourceMappingURL=index.647bb851.js.map
+
+//# sourceMappingURL=index.647bb851.js.map
---
+++ docs/index.b5578f08.js
@@ -0,0 +1,6 @@
+scrollArea=document.getElementById("scrollArea"),// Call the animate function to start the animation loop
+// Set up an animation loop
+function e(){var l;console.log(l=new Date().getTime()),scrollArea.style.right=l/50%3600+"px",// Call the animate function again next time the browser is ready to draw
+requestAnimationFrame(e)}();//# sourceMappingURL=index.b5578f08.js.map
+
+//# sourceMappingURL=index.b5578f08.js.map
