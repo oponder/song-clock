c81f133beddf8b8afe1da6af1cbe24acb913b4f0
Oliver Ponder <oliver.ponder@gmail.com>
2023-09-22 21:19:12 +0200

    Adjust some things

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -575,6 +575,65 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"bNKaB":[function(require,module,exports) {
 scrollArea = document.getElementById("scrollArea");
+circleArea = document.getElementById("circleArea");
+// Circle of fifths
+const circle = [
+    [
+        "C",
+        "Am"
+    ],
+    [
+        "G",
+        "Em"
+    ],
+    [
+        "D",
+        "Bm"
+    ],
+    [
+        "A",
+        "F#m"
+    ],
+    [
+        "E",
+        "C#m"
+    ],
+    [
+        "B",
+        "G#m"
+    ],
+    [
+        "F#",
+        "D#m"
+    ],
+    [
+        "C#",
+        "A#m"
+    ],
+    [
+        "Ab",
+        "Fm"
+    ],
+    [
+        "Eb",
+        "Cm"
+    ],
+    [
+        "Bb",
+        "Gm"
+    ],
+    [
+        "F",
+        "Dm"
+    ]
+];
+// Create a div for each key
+circle.forEach((key)=>{
+    let div = document.createElement("div");
+    div.classList.add("key");
+    div.innerHTML = key[0] + " / " + key[1];
+    circleArea.appendChild(div);
+});
 // Set up an animation loop
 const startTime = new Date().getTime();
 function animate() {
---
+++ docs/index.0614b0c0.js
@@ -0,0 +1,6 @@
+let e;scrollArea=document.getElementById("scrollArea"),circleArea=document.getElementById("circleArea"),// Create a div for each key
+[["C","Am"],["G","Em"],["D","Bm"],["A","F#m"],["E","C#m"],["B","G#m"],["F#","D#m"],["C#","A#m"],["Ab","Fm"],["Eb","Cm"],["Bb","Gm"],["F","Dm"]].forEach(e=>{let t=document.createElement("div");t.classList.add("key"),t.innerHTML=e[0]+" / "+e[1],circleArea.appendChild(t)}),new Date().getTime(),e=window.innerWidth,addEventListener("resize",t=>{e=window.innerWidth}),// Call the animate function to start the animation loop
+function t(){var n;n=new Date().getTime(),scrollArea.style.right=n%3e5*2/3e5*e+"px",// Call the animate function again next time the browser is ready to draw
+requestAnimationFrame(t)}();//# sourceMappingURL=index.0614b0c0.js.map
+
+//# sourceMappingURL=index.0614b0c0.js.map
